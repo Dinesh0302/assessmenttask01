f56c5c29e97f8a6bf320da4c2cbe20b8ddb20a9e
Dinesh0302 <dinusjec@gmail.com>
2023-09-14 12:30:13 +0400

    update fetchuser
    update fetchuser

--- src/app/users/fetchUsers.tsx
+++ src/app/users/fetchUsers.tsx
@@ -1,7 +1,20 @@
-export const fetchUsers = async (currentPage : any) => {
+// Define a type for the user data structure if not already defined
+interface UserData {
+  // Define the properties of a user
+  userId: number;
+  username: string;
+  email: string;
+  password: string;
+  dob: string;
+  assessmentSubmitted: boolean;
+  picture: string;
+}
+
+export const fetchUsers = async (currentPage: number): Promise<UserData[]> => {
   const res = await fetch(`http://localhost:3000/api/users?page=${currentPage}`);
   if (!res.ok) {
     throw new Error('Failed to fetch user data');
- }
- return res.json();
-}
+  }
+  const data: UserData[] = await res.json();
+  return data;
+};
