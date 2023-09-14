ac98cfbfae036b71967e40a795a67f3b40d29486
Dinesh0302 <dinusjec@gmail.com>
2023-09-14 12:48:30 +0400

    update fetchuser
    update fetchuser

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45101a2dcebe0f98"; }
+/******/ 		__webpack_require__.h = function() { return "91d863bc69f06c30"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/users/fetchUsers.tsx
+++ src/app/users/fetchUsers.tsx
@@ -1,7 +1,18 @@
-export const fetchUsers = async (currentPage : any) => {
+interface UserData {
+  userId: number;
+  username: string;
+  email: string;
+  password: string;
+  dob: string;
+  assessmentSubmitted: boolean;  // true/false
+  picture: string;
+}
+
+export const fetchUsers = async (currentPage: number): Promise<UserData[]> => {
   const res = await fetch(`http://localhost:3000/api/users?page=${currentPage}`);
   if (!res.ok) {
     throw new Error('Failed to fetch user data');
- }
- return res.json();
-}
+  }
+  const data: UserData[] = await res.json();
+  return data;
+};
